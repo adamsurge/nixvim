958b64961123a40f17a0d33a3e86a2b0634eed01
Adam S <adiosurgio@gmail.com>
2025-10-22 13:57:27 +1100

    Fix startup page

--- config/plugins/ui/startup.nix
+++ config/plugins/ui/startup.nix
@@ -1,72 +1,77 @@
 {
   plugins.startup = {
     enable = true;
-    settings = {
-      theme = "dashboard";
 
+    settings = {
       colors = {
         background = "#ffffff";
         foldedSection = "#ffffff";
       };
 
-      sections = {
-        header = {
-          type = "text";
-          oldfilesDirectory = false;
-          align = "center";
-          foldSection = false;
-          title = "Header";
-          margin = 5;
-          content = [
-            " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ "
-            " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ "
-            " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ "
-            " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
-            " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ "
-            " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
-          ];
-          highlight = "Statement";
-          defaultColor = "";
-          oldfilesAmount = 0;
-        };
+      header = {
+        type = "text";
+        oldfilesDirectory = false;
+        align = "center";
+        foldSection = false;
+        title = "Header";
+        margin = 5;
+        content = [
+          " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ "
+          " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ "
+          " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ "
+          " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
+          " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ "
+          " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
+        ];
+        highlight = "Statement";
+        defaultColor = "";
+        oldfilesAmount = 0;
+      };
 
-        body = {
-          type = "mapping";
-          oldfilesDirectory = false;
-          align = "center";
-          foldSection = false;
-          title = "Menu";
-          margin = 5;
-          content = [
-            [
-              " Find File"
-              "Telescope find_files"
-              "ff"
-            ]
-            [
-              "󰍉 Find Word"
-              "Telescope live_grep"
-              "fr"
-            ]
-            [
-              " Recent Files"
-              "Telescope oldfiles"
-              "fg"
-            ]
-            [
-              " File Browser"
-              "Telescope file_browser"
-              "fe"
-            ]
-          ];
-          highlight = "string";
-          defaultColor = "";
-          oldfilesAmount = 0;
-        };
+      body = {
+        type = "mapping";
+        oldfilesDirectory = false;
+        align = "center";
+        foldSection = false;
+        title = "Menu";
+        margin = 5;
+        content = [
+          [
+            " Find File"
+            "Telescope find_files"
+            "ff"
+          ]
+          [
+            "󰍉 Find Word"
+            "Telescope live_grep"
+            "fr"
+          ]
+          [
+            " Recent Files"
+            "Telescope oldfiles"
+            "fg"
+          ]
+          [
+            " File Browser"
+            "Telescope file_browser"
+            "fe"
+          ]
+          [
+            " Keymaps"
+            "Telescope keymaps"
+            "sk"
+          ]
+        ];
+        highlight = "string";
+        defaultColor = "";
+        oldfilesAmount = 0;
       };
 
       options = {
-        paddings = [1 3];
+        paddings = [
+          1
+          3
+        ];
       };
 
       parts = [
