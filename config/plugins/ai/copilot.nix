7559d2715c46b00af460242f294c6efd3150bb01
Adam S <adiosurgio@gmail.com>
2025-10-30 14:29:18 +1100

    Add global Copilot toggle commands
    Introduce :CopilotEnable, :CopilotDisable, and :CopilotToggle as global
    commands. They update vim.g.copilot_enabled which is used to disable
    Copilot automatically via an autocmd.

--- config/plugins/ai/copilot.nix
+++ config/plugins/ai/copilot.nix
@@ -17,4 +17,61 @@
       };
     };
   };
+
+  autoGroups = {
+    copilot_control = {};
+  };
+
+  autoCmd = [
+    {
+      group = "copilot_control";
+      event = ["VimEnter"];
+      pattern = "*";
+      callback = {
+        __raw = ''
+          function()
+            if not vim.g.COPILOT_ENABLED then
+              vim.cmd("Copilot disable")
+              print("Copilot disabled on startup")
+            end
+          end
+        '';
+      };
+    }
+  ];
+
+  extraConfigLua = ''
+    -- CopilotEnable: enable Copilot suggestions globally
+    vim.api.nvim_create_user_command("CopilotEnable", function()
+      vim.g.COPILOT_ENABLED = true
+      vim.cmd("Copilot enable")
+      vim.notify("Copilot enabled globally", vim.log.levels.INFO)
+    end, {
+      desc = "Enable Copilot suggestions globally",
+    })
+
+    -- CopilotDisable: disable Copilot suggestions globally
+    vim.api.nvim_create_user_command("CopilotDisable", function()
+      vim.g.COPILOT_ENABLED = false
+      vim.cmd("Copilot disable")
+      vim.notify("Copilot disabled globally", vim.log.levels.INFO)
+    end, {
+      desc = "Disable Copilot suggestions globally",
+    })
+
+    -- CopilotToggle: flip Copilot state globally
+    vim.api.nvim_create_user_command("CopilotToggle", function()
+      if vim.g.COPILOT_ENABLED then
+        vim.g.copilot_enabled = false
+        vim.cmd("Copilot disable")
+        vim.notify("Copilot disabled globally", vim.log.levels.INFO)
+      else
+        vim.g.cOPILOT_ENABLED = true
+        vim.cmd("Copilot enable")
+        vim.notify("Copilot enabled globally", vim.log.levels.INFO)
+      end
+    end, {
+      desc = "Toggle Copilot suggestions globally",
+    })
+  '';
 }
