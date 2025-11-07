3846672d24762ed10d6402b714740577c4a593e1
Adam S <adiosurgio@gmail.com>
2025-11-07 14:59:35 +1100

    Add snacks rename with file manager integrations

--- config/plugins/utils/snacks.nix
+++ config/plugins/utils/snacks.nix
@@ -200,6 +200,7 @@
         ];
       };
       lazygit.enabled = true;
+      rename.enabled = true;
     };
   };
 
@@ -398,4 +399,35 @@
       options = {desc = "Lazygit";};
     }
   ];
+
+  extraConfigLua = ''
+    -- Integration with oil.nvim for rename functionality
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if event.data.actions[1].type == "move" then
+          Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+        end
+      end,
+    })
+
+    -- Integration with neo-tree for rename functionality
+    local events = require("neo-tree.events")
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = events.FILE_MOVED,
+          handler = function(data)
+            Snacks.rename.on_rename_file(data.source, data.destination)
+          end,
+        },
+        {
+          event = events.FILE_RENAMED,
+          handler = function(data)
+            Snacks.rename.on_rename_file(data.source, data.destination)
+          end,
+        },
+      },
+    })
+  '';
 }
