ffe3eaace652b3542e3c5bcb05a974aafc328faf
Adam S <adiosurgio@gmail.com>
2025-11-07 15:51:20 +1100

    Add snacks git/github integration

--- config/plugins/utils/snacks.nix
+++ config/plugins/utils/snacks.nix
@@ -1,8 +1,17 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  extraPackages = with pkgs; [
+    gh
+  ];
+
   plugins.snacks = {
     enable = true;
     settings = {
       input.enabled = true;
+      quickfile.enabled = true;
       picker = {
         enabled = true;
         layout = {
@@ -200,6 +209,8 @@
         ];
       };
       lazygit.enabled = true;
+      git.enabled = true;
+      github.enabled = true;
       rename.enabled = true;
       statuscolumn = {
         enabled = true;
@@ -402,6 +413,60 @@
       key = "<leader>gg";
       action = "<cmd>lua Snacks.lazygit()<CR>";
       options = {desc = "Lazygit";};
+    }
+    # Additional Git keymaps
+    {
+      mode = "n";
+      key = "<leader>g";
+      action = "";
+      options = {
+        desc = "git";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = "<cmd>lua Snacks.picker.git_branches()<CR>";
+      options = {desc = "Git Branches";};
+    }
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action = "<cmd>lua Snacks.picker.git_diff()<CR>";
+      options = {desc = "Git Diff";};
+    }
+    {
+      mode = "n";
+      key = "<leader>gS";
+      action = "<cmd>lua Snacks.picker.git_stash()<CR>";
+      options = {desc = "Git Stash";};
+    }
+    # GitHub CLI keymaps
+    {
+      mode = "n";
+      key = "<leader>gh";
+      action = "";
+      options = {
+        desc = "GitHub";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ghi";
+      action = "<cmd>lua Snacks.terminal('gh issue list')<CR>";
+      options = {desc = "GitHub Issues";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ghp";
+      action = "<cmd>lua Snacks.terminal('gh pr list')<CR>";
+      options = {desc = "GitHub Pull Requests";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ghr";
+      action = "<cmd>lua Snacks.terminal('gh repo list')<CR>";
+      options = {desc = "GitHub Repos";};
     }
   ];
 
